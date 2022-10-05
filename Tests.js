326bfc8ee1d992b1b99a723c3c38124b8b049f05
TrungVmo <trungnd@vmodev.com>
2022-10-05 22:56:12 +0700

    ffff

---
+++ Tests.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Tests = () => {
+    return (
+        <div>
+            <p>this is 1</p>
+            <p>this is 2</p>
+            <p>this is 3</p>
+        </div>
+    );
+};
+
+export default Tests;
